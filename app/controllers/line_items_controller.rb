b41083024fa4963a63910d6288bb8a9ba0ec8885
narakai <narakai@126.com>
2014-12-14 22:14:56 +0800

    modified cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,4 +1,8 @@
 class LineItemsController < ApplicationController
+  include CurrentCart
+  #call set_cart method before create action
+  before_action :set_cart, only: [:create]
+  #call set_cart method before ... 
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
@@ -24,15 +28,16 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    @line_item = LineItem.new(line_item_params)
+    product = Product.find(params[:product_id])
+    @line_item = @cart.line_items.build(product: product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.json { render jsoon: @line_item.errors, status: :unprocessable_entity }
       end
     end
   end
