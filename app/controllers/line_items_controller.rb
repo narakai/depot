b7f6b1369ad2eae9bf343c0925b6f537b6da8683
narakai <narakai@126.com>
2014-12-15 09:40:02 +0800

    update controller

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,4 +1,8 @@
 class LineItemsController < ApplicationController
+
+  include CurrentCart
+  before_action :set_cart, only: [:create]
+
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
 
   # GET /line_items
