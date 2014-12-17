fa12b4ad1af5290fe59d31f0758c0a88883a69f6
narakai <narakai@126.com>
2014-12-17 15:10:25 +0800

    final done app

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -4,7 +4,8 @@ class LineItemsController < ApplicationController
   before_action :set_cart, only: [:create]
   #call set_cart method before ... 
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
-
+  skip_before_action :authorize, only: [:new, :create]
+  
   # GET /line_items
   # GET /line_items.json
   def index
